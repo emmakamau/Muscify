f1e46ce8a467cc1c7dd0a0b717260c49f15d90e5
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-17 21:14:16 +0300

    feature:adding a registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Enter Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators = [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
