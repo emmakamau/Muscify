35600192ef78492d235fc778ac088748f8b9c30b
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-17 21:24:06 +0300

    adding a function to log out an existing user

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,3 +30,9 @@ def login():
 
     title = "Muscify login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
