70fe38bfcec5aa48e95f98af973ac219cd89cfa0
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-17 21:22:55 +0300

    adding a function to register a new user

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,18 @@ from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
