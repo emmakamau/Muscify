35797b977703e47e86017f2c2b7a8c54190f7561
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-18 15:24:39 +0300

    comment out migrate command

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from flask_migrate import Migrate #version 2.6.0
+# from flask_migrate import Migrate #version 2.6.0
 from app.models import *
 
 # Creating app instance
@@ -11,8 +11,8 @@ app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db')
+# migrate = Migrate(app,db)
+# manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
