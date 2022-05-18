ae08b06773cb493260fe5d96913870a924927a49
EmmaK <emmaculatewkamau@gmail.com>
2022-05-18 12:21:16 +0300

    Merge with ft-request

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..request import *
 
 @main.route('/')
 def index(): 
@@ -13,14 +13,9 @@ def discover():
    title='Discover'
 
    return render_template('discover.html',title=title)
-=======
-from . import main
-from flask import render_template
-from ..request import *
 
 @main.route('/')
 def index():
     allCharts = getchart()
 
     return render_template('index.html',charts=allCharts)
->>>>>>> 7a05275dc11ba247bf01b1b05f3344a960b14366
