c0b858f4966a5e02718de0e8e73b9320be9cc4c1
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-18 12:13:56 +0300

    create a review model class

--- config.py
+++ config.py
@@ -3,14 +3,10 @@ import os
 
 class Config:
     password = os.environ.get('SQL_PASSWORD')
-<<<<<<< HEAD
+
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorraine:gift1234@localhost/muscify'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-=======
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:GenZ|0420@localhost/muscify'
     SECRET_KEY = os.environ.get('SECRET_KEY')
->>>>>>> 3cbe3e1109f8c04a547d85be025f6f851b5189d4
+
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     #  email configurations
@@ -22,24 +18,18 @@ class Config:
     pass
 
 class ProdConfig(Config):
-<<<<<<< HEAD
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
-=======
     #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
->>>>>>> 3cbe3e1109f8c04a547d85be025f6f851b5189d4
+
     pass
 
 class DevConfig(Config):
     DEBUG = True
 
 class TestConfig(Config):
-<<<<<<< HEAD
+
     # password = os.environ.get('SQL_PASSWORD')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:GenZ|0420@localhost/blog_test'
-=======
-    password = os.environ.get('SQL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:GenZ|0420@localhost/muscify_test'
->>>>>>> 3cbe3e1109f8c04a547d85be025f6f851b5189d4
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:GenZ|0420@localhost/muscify_test'
+
     pass
 
 config_options = {
