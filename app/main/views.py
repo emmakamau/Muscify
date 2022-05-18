0247ca262f3e0f268f4a08aad07a8cf4393968a9
EmmaK <emmaculatewkamau@gmail.com>
2022-05-18 06:51:13 +0300

    Add discover page

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,9 @@ def index():
    title="Homepage"
    
    return render_template('index.html',title=title)
+
+@main.route('/discover')
+def discover():
+   title='Discover'
+
+   return render_template('discover.html',title=title)
